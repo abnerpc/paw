322c4653efd646c53f1c51694947c02aa07078f8
Abner Campanha <abnerpc@gmail.com>
2018-03-20 12:14:54 -0300

    added initial project structure

---
+++ Makefile
@@ -0,0 +1,32 @@
+APP_PATH=src/paw
+MANAGE_CMD=python $(APP_PATH)/manage.py
+
+clean:
+	@find . -name "*.pyc" | xargs rm -rf
+	@find . -name "*.pyo" | xargs rm -rf
+	@find . -name "__pycache__" -type d | xargs rm -rf
+	@rm -rf build
+
+install:
+	python setup.py install
+
+install-dev:
+	pip install -r requirements-dev.txt
+
+.env:
+	cp contrib/env .env
+
+run: .env clean
+	$(MANAGE_CMD) runserver
+
+migrate: .env
+	$(MANAGE_CMD) migrate
+
+superuser: .env
+	$(MANAGE_CMD) createsuperuser
+ 
+test: clean
+	pytest
+
+test-cov: clean
+	pytest --cov-report term-missing --cov=paw
---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='paw',
+    version='0.0.0',
+    license='BSD 2-Clause License',
+    description='A well-defined API to calculate phone calls costs',
+    long_description='%s\n%s' % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.md'))
+    ),
+    author='Abner Paulo Campanha',
+    author_email='abnerpc@gmail.com',
+    url='https://github.com/abnerpc/paw',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'django==2.0.3',
+        'python-decouple==3.1',
+        'psycopg2-binary==2.7.4'
+    ],
+)
